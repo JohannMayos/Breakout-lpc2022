f8514f1385c23de7ff9bc9a518f68920138fcdb6
JulioCFSdev <90019843+JulioCFSdev@users.noreply.github.com>
2022-02-19 17:39:01 -0300

    Otimizando os draws dos blocos e da bola e adicionando a colisão com blocos

--- Breakout Game.py
+++ Breakout Game.py
@@ -16,61 +16,6 @@ block_green = (14, 209, 69)
 block_yellow = (255, 242, 0)
 background_color = (0, 0, 0)
 
-rows = 8
-columns = 14
-
-class blocks():
-    
-    # define width and height for blocks
-    def __init__(self):
-        self.width = 49
-        self.height = 20
-
-    # fuction to set block form, coordinate and your colors 
-    def create_wall(self):
-        self.blocks = []
-        block_individual = []
-
-        for row in range(rows):
-            block_row = []
-
-            for column in range(columns):
-                block_x = column * self.width + 15
-                block_y = row * self.height + 152
-                rect = pygame.Rect(block_x, block_y, self.width, self.height)
-
-                if row == 0 or row == 1:
-                    color = 4
-                elif row == 2 or row == 3:
-                    color = 3
-                elif row == 4 or row == 5:
-                    color = 2
-                elif row == 6 or row == 7:
-                    color = 1
-
-                block_individual = [rect, color]
-                block_row.append(block_individual)
-            self.blocks.append(block_row)
-
-
-    # fuction to draw the blocks
-    def draw_wall_blocks(self):
-        for row in self.blocks:
-            for block in row:
-                if block[1] == 4:
-                    block_col = block_red
-                elif block[1] == 3:
-                    block_col = block_orange
-                elif block[1] == 2:
-                    block_col = block_green
-                elif block[1] == 1:
-                    block_col = block_yellow
-                pygame.draw.rect(screen, block_col, block[0])
-                pygame.draw.rect(screen, background_color, (block[0]), 2)
-
-wall = blocks()
-wall.create_wall()
-
 # drawing paddle
 paddle = pygame.image.load("image/paddle.png")
 paddle_x = 300
@@ -78,11 +23,43 @@ paddle_move_left = False
 paddle_move_right = False
 
 # ball
-ball = pygame.image.load("image/Ball.png")
-ball_x = 300
-ball_y = 300
+ball = pygame.Rect(400, 400, 15, 15)
+ball_x = 400
+ball_y = 400
 ball_dx = 3
-ball_dy = 3
+ball_dy = -3
+ball = pygame.Rect(ball_x, ball_y, 15, 15)
+
+block_width = 49
+block_height = 20
+rows = 8
+cols = 14
+block_list = []
+
+def create_wall():
+    block_individual = []
+
+    for row in range(rows):
+        for col in range(cols):
+            block_x = col * block_width + 15
+            block_y = row * block_height + 152
+
+            if row == 0 or row == 1:
+                    color = block_red
+                    score = 7
+            elif row == 2 or row == 3:
+                    color = block_orange
+                    score = 5
+            elif row == 4 or row == 5:
+                    color = block_green
+                    score = 3
+            elif row == 6 or row == 7:
+                    color = block_yellow
+                    score = 1
+
+            block = pygame.Rect(block_x, block_y, block_width, block_height)
+            block_individual = [block, color, score]
+            block_list.append(block_individual)
 
 # score text
 score_font = pygame.font.Font('breakout.ttf', 44)
@@ -98,12 +75,13 @@ score_2 = 0
 game_loop = True
 game_clock = pygame.time.Clock()
 
+create_wall()
+
 while game_loop:
     
     # clear screen and set background again
     screen.fill(background_color)
     screen.blit(background,(0,0))
-    wall.draw_wall_blocks()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -122,36 +100,42 @@ while game_loop:
                 paddle_move_right = False
 
     # ball movement
-    ball_x = ball_x + ball_dx
-    ball_y = ball_y + ball_dy
+    ball.x += ball_dx
+    ball.y += ball_dy
+
+    for block in block_list:
+        block_count = 0
+        if ball.colliderect(block[0]):
+            ball_dy *= -1
+            block_list[block_count] = (0, 0, 0) 
 
     # ball collision with the paddle
-    if ball_y >= 605:
-        if paddle_x < ball_x + 80:
-            if paddle_x + 80 > ball_x:
+    if ball.y >= 605:
+        if paddle_x < ball.x + 80:
+            if paddle_x + 80 > ball.x:
                 ball_dy *= -1
                 ball_dx *= 1
 
     # ball´s death point
-    if ball_y > 650:
-        ball_x = 300
-        ball_y = 300
+    if ball.y > 650:
+        ball.x = 300
+        ball.y = 300
         ball_dx *= -1
         ball_dy *= 1
         game_clock.tick(5)
 
     # ball collision with right wall
-    if ball_x > 680:
+    if ball.x > 680:
         ball_dx *= -1
         ball_dy *= 1
 
     # ball collision with upper wall
-    if ball_y <= 0:
+    if ball.y <= 0:
         ball_dx *= 1
         ball_dy *= -1
 
     # ball collision with left wall
-    if ball_x == 0:
+    if ball.x == 1:
         ball_dx *= -1
         ball_dy *= 1
 
@@ -176,10 +160,14 @@ while game_loop:
         paddle_x += 0
 
     # draw objects
-    screen.blit(ball, (ball_x, ball_y))
+    pygame.draw.ellipse(screen, (255, 255, 255), ball)
     screen.blit(paddle, (paddle_x, 625))
     screen.blit(score_text, score_text_rect)
 
+    for block in block_list:
+        pygame.draw.rect(screen, block[1], block[0])
+        pygame.draw.rect(screen, background_color, (block[0]), 2)
+            
     # update screen
     pygame.display.flip()
     game_clock.tick(60)
